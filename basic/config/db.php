54ea2c7cd240ee097f8f1075f39a2af44b0a064c
git-tomas <gootomas@gmail.com>
2016-12-05 19:22:51 +0100

    Configuración Conexión BD.

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=daw_tienda',
+    'dsn' => 'mysql:host=localhost;dbname=daw2_actividades',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
