268da97c7417adc6b827796edd80e4ae74e18a81
git-tomas <gootomas@gmail.com>
2016-12-13 19:19:07 +0100

    Poner establecer la configuración de la Base de Datos para cada servidor personal o en internet.

---
+++ basic/config/db_defecto.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=daw2_actividades',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
