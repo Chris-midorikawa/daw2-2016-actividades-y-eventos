39a02263d1829877379a8e955e8097966bb6ede7
Chris-midorikawa <cris.a.gorin@usal.es>
2017-01-12 00:28:30 +0100

    Add files via upload

--- basic/views/site/login.php
+++ basic/views/site/login.php
@@ -1,8 +1,6 @@
 <?php
-
-
-
 use app\models\Usuarios;
+use app\models\User;
 use app\models\UsuariosSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -47,8 +45,4 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php ActiveForm::end(); ?>
 
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
 </div>
