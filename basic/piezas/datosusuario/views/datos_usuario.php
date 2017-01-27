28f200ee281996649e340dc060403597c03163a5
ernesto-usal <ernesto.boado@usal.es>
2017-01-27 12:31:31 +0100

    Resubido anterior commit con métodos de usuarios.

--- basic/piezas/datosusuario/views/datos_usuario.php
+++ basic/piezas/datosusuario/views/datos_usuario.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use kartik\detail\DetailView;
 
 /* Variables disponibles:
         $modelo_usuario
@@ -8,5 +9,19 @@ use yii\helpers\Html;
 ?>
 
 <div>
-    <h4>CONTENIDO DE WIDGET DE DATOS DE USUARIO</h4>
+    <?= DetailView::widget([
+        'model'=>$modelo_usuario,
+        'condensed'=>true,
+        'hover'=>true,
+        'mode'=>DetailView::MODE_VIEW,
+        'panel'=>[
+            'heading'=>'Book # ' . $modelo_usuario->id,
+            'type'=>DetailView::TYPE_INFO,
+        ],
+        'attributes'=>[
+            'id',
+            'nombre',
+            ['attribute'=>'fecha_nacimiento', 'type'=>DetailView::INPUT_DATE],
+        ]
+    ]); ?>
 </div>
