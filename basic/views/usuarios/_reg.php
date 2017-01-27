18249dc4f8efe297f2b2a7874d9451a9146114aa
Chris-midorikawa <cris.a.gorin@usal.es>
2017-01-27 02:00:31 +0100

    Registro para usuarios normales, valores por defecto, corregido date compatible con sql

--- basic/views/usuarios/_reg.php
+++ basic/views/usuarios/_reg.php
@@ -5,31 +5,37 @@ use app\models\Usuarios;
 
 
 
-/* @var $this yii\web\View */
-/* @var $model app\models\Usuarios */
-/* @var $form yii\widgets\ActiveForm */
+/* MODELO REGISTRO PARA USUARIOS NORMALES*/
+
+
 ?>
 
 <div class="usuarios-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
-
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true,'hidden'=> true]) ?>
     <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
-
     <?= $form->field($model, 'direccion')->textarea(['rows' => 6]) ?>
-   
+    <?= $form->field($model, 'area_id')->hiddenInput(['value' => 2 ])->label(false);?>
+    <?= $form->field($model, 'rol')->dropDownList(usuarios::listaroles(),(['maxlength' => true]))->hiddenInput(['value' => 'N'])->label(false);?>
+    <?= $form->field($model, 'avisos_por_correo')->checkbox($options = []) ?>
+    <?= $form->field($model, 'avisos_agrupados')->checkbox($options = []) ?>
+    <?= $form->field($model, 'avisos_marcar_leidos')->textInput(['value' => 0]) ?>
+    <?= $form->field($model, 'avisos_eliminar_borrados')->textInput(['value' => 0]) ?>
+    <?= $form->field($model, 'fecha_registro')->hiddenInput(['value' => date("Y-m-d h:i:sa")])->label(false);?>
+    <?= $form->field($model, 'confirmado')->hiddenInput(['value' => 0])->label(false);?>
+    <?= $form->field($model, 'fecha_acceso')->hiddenInput(['value' => date("Y-m-d h:i:sa")])->label(false);?>
+    <?= $form->field($model, 'num_accesos')->hiddenInput(['value' => 1])->label(false);?>
+    <?= $form->field($model, 'bloqueado')->hiddenInput(['value' => 0])->label(false);?>
+    <?= $form->field($model, 'fecha_bloqueo')->hiddenInput(['value' => ''])->label(false);?>
+    <?= $form->field($model, 'notas_bloqueo')->hiddenInput(['value' => ''])->label(false);?>
  <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Crear Usuario' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton('Modificar', ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Volver'), ['/site/login'], ['class' => 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end(); ?>
